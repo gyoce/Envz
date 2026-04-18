9c1152dc78f937c9c858e9a0d47f051306739af3
gyoce <schoefolt.guillaume@gmail.com>
2026-04-18 21:19:15 +0200

    add styles & start design

---
+++ EnvBooster.UI/App.xaml.cs
@@ -0,0 +1,7 @@
+﻿using System.Windows;
+
+namespace EnvBooster.UI;
+
+public partial class App : Application
+{
+}
---
+++ EnvBooster.UI/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EnvBooster.UI;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void CloseButtonClick(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void MinimizeButtonClick(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void TitleBarMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+}
