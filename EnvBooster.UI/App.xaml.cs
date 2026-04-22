bd8ce8f897b3e18b3f4b82e4c0ba17eacf1cdc86
gyoce <schoefolt.guillaume@gmail.com>
2026-04-22 22:09:36 +0200

    add start dependency injection

--- EnvBooster.UI/App.xaml.cs
+++ EnvBooster.UI/App.xaml.cs
@@ -1,7 +1,25 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EnvBooster.UI;
 
 public partial class App : Application
 {
+    private IServiceProvider? ServiceProvider { get; set; }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        ServiceCollection serviceCollection = ConfigureServices();
+        ServiceProvider = serviceCollection.BuildServiceProvider();
+
+        MainWindow mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    private static ServiceCollection ConfigureServices()
+    {
+        ServiceCollection services = new();
+        services.AddUi();
+        return services;
+    }
 }
---
+++ EnvBooster.UI/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace EnvBooster.UI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddUi(this IServiceCollection services)
+    {
+        services.AddSingleton<MainWindow>();
+        return services;
+    }
+}
