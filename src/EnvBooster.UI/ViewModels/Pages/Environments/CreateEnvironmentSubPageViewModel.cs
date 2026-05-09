a87732300ed7fff373a3be589256f88410369874
gyoce <schoefolt.guillaume@gmail.com>
2026-05-09 16:10:53 +0200

    add select application window dialog

--- src/EnvBooster.UI/ViewModels/Pages/Environments/CreateEnvironmentSubPageViewModel.cs
+++ src/EnvBooster.UI/ViewModels/Pages/Environments/CreateEnvironmentSubPageViewModel.cs
@@ -4,6 +4,7 @@ using System.Windows.Input;
 using EnvBooster.Application.Environments;
 using EnvBooster.UI.Utils;
 using EnvBooster.UI.ViewModels.UserControls;
+using EnvBooster.UI.Views;
 
 namespace EnvBooster.UI.ViewModels.Pages.Environments;
 
@@ -11,11 +12,14 @@ public class CreateEnvironmentSubPageViewModel : ViewModelBase
 {
     public ICommand CreateEnvironmentCommand { get; }
     public ICommand CancelCreateEnvironmentCommand { get; }
+    public ICommand AddApplicationCommand { get; }
     public ObservableCollection<EnvironmentApplicationViewModel> EnvironmentApplications { get; } = [];
     public CreateEnvironmentRequest Request { get; set; } = new();
+    public bool HasNoApplications => EnvironmentApplications.Count == 0;
 
     public CreateEnvironmentSubPageViewModel(CreateEnvironmentUseCase createEnvironmentUseCase)
     {
+        EnvironmentApplications.CollectionChanged += (_, _) => OnPropertyChanged(nameof(HasNoApplications));
         CancelCreateEnvironmentCommand = new RelayCommand(_ =>
         {
             Request = new CreateEnvironmentRequest();
@@ -26,6 +30,14 @@ public class CreateEnvironmentSubPageViewModel : ViewModelBase
             createEnvironmentUseCase.Execute(Request);
             Messenger.NotifyEnvironmentCreated();
             Messenger.NotifyNavigationToHomeEnvironments();
+        });
+        AddApplicationCommand = new RelayCommand(_ =>
+        {
+            SelectApplicationDialog dialog = new()
+            {
+                Owner = System.Windows.Application.Current.MainWindow
+            };
+            dialog.ShowDialog();
         });
     }
 }
---
+++ src/EnvBooster.UI/Views/SelectApplicationDialog.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EnvBooster.UI.Views;
+
+public partial class SelectApplicationDialog : Window
+{
+    public SelectApplicationDialog()
+    {
+        InitializeComponent();
+    }
+
+    public void CloseButtonClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+        Close();
+    }
+
+    private void TitleBarMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        DragMove();
+    }
+}
